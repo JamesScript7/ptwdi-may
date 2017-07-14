14a9e670a7944f48c7fb0e15548e60f2a4732150
James Kim <james.kim7@yahoo.com>
2017-07-14 01:25:41 -0400

    Finished part 2 of country project

--- projects/countryinfo/index.js
+++ projects/countryinfo/index.js
@@ -2,28 +2,14 @@
 
 const fs = require('fs');
 
-//Module Example:
-//const jsonReader = require('./json-file-reader.js');
-
 //The name of the country taken from the command line.
 var input = process.argv[2];
 var file = './countries.json';
 
-fs.readFile(file, function(err, data){
+//Part 2 module example.
+var module = require('./json-file-reader.js');
+
+module.jsonReader(file, input, function(err, data) {
   if (err) throw err;
-
-  //Remember: Array of OBJECTS
-  var country = JSON.parse(data);
-
-  country.forEach((obj)=>{
-
-    if (input === obj.name) {
-      for (var key in obj) {
-        console.log(`${key.charAt(0).toUpperCase() + key.slice(1)}: ${obj[key]}`);
-      }
-    }
-
-  });
-
-
+  console.log(data);
 });
--- projects/countryinfo/json-file-reader.js
+++ projects/countryinfo/json-file-reader.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function reader(fileName, callback) {
-  console.log("Hello from the reader!");
+var fs = require('fs');
+
+function jsonReader(file, input, callback) {
+  callback(
+    fs.readFile(file, function(err, data) {
+      if (err) throw err;
+      var country = JSON.parse(data);
+      country.forEach((obj) => {
+        //Utilize .toLowerCase() so capitalization doesn't matter.
+        if (input === obj.name) {
+          for(let key in obj) {
+            console.log(`${key.charAt(0).toUpperCase()}${key.slice(1)}: ${obj[key]}`);
+          }
+        }
+
+      });
+    })
+
+  );
 }
 
-module.exports = reader
+
+
+module.exports = {
+  jsonReader: jsonReader
+};
